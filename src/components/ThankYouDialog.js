8282344a34cf89f65849c24e13e155aa1c6f509b
Grant Fuellenbach <grant.fuellenbach@gmail.com>
2024-06-26 10:09:10 -0600

    Create ThankYouDialog.js

---
+++ src/components/ThankYouDialog.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AlertDialog, AlertDialogAction, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from '@/components/ui/alert-dialog';
+
+const ThankYouDialog = ({ open, onOpenChange }) => (
+  <AlertDialog open={open} onOpenChange={onOpenChange}>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Thank You!</AlertDialogTitle>
+        <AlertDialogDescription>
+          Your progress has been saved. We'll send you updates and tips to help you generate more leads for your drone business.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogAction onClick={() => onOpenChange(false)}>Close</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+);
+
+export default ThankYouDialog;
