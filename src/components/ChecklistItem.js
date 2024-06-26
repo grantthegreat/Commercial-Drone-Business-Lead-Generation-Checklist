a9bd51b8478be492a0e9e02da314255096566328
Grant Fuellenbach <grant.fuellenbach@gmail.com>
2024-06-26 10:07:57 -0600

    Create ChecklistItem.js

---
+++ src/components/ChecklistItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Checkbox } from '@/components/ui/checkbox';
+
+const ChecklistItem = ({ item, onToggle, isChecked }) => (
+  <div className="flex items-center space-x-2 mb-2">
+    <Checkbox id={item} checked={isChecked} onCheckedChange={() => onToggle(item)} />
+    <label htmlFor={item} className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+      {item}
+    </label>
+  </div>
+);
+
+export default ChecklistItem;
