65cedfd042ff3c22155d999f191fb88d554dcd19
Grant Fuellenbach <grant.fuellenbach@gmail.com>
2024-06-26 10:09:36 -0600

    Create checklistData.js

---
+++ src/data/checklistData.js
@@ -0,0 +1,34 @@
+export const sections = {
+  'Content Marketing': [
+    'Create blog posts on drone applications and industry trends',
+    'Develop e-books on drone technology advancements',
+    'Produce whitepapers on drone regulations and compliance',
+    'Design infographics showcasing drone capabilities',
+    'Record video tutorials on drone operation and maintenance'
+  ],
+  'Email Marketing': [
+    'Set up weekly or bi-weekly newsletter',
+    'Create automated email sequences for new subscribers',
+    'Segment email lists based on industry and interests',
+    'Send periodic special offers or promotions'
+  ],
+  'Social Media': [
+    'Maintain regular posts on LinkedIn, Twitter, and Facebook',
+    'Create engaging visual content for Instagram and YouTube',
+    'Participate in relevant LinkedIn groups and Twitter chats',
+    'Run paid social media advertising campaigns',
+    'Implement an employee advocacy program'
+  ],
+  'Webinars and Virtual Events': [
+    'Host monthly webinars on drone applications',
+    'Conduct Q&A sessions with drone experts',
+    'Offer virtual product demonstrations',
+    'Organize industry panel discussions'
+  ],
+  'Lead Magnets': [
+    'Create a free drone flight planning template',
+    'Develop an ROI calculator for drone services',
+    'Produce checklists for drone regulations compliance',
+    'Design a mini-course on getting started with commercial drones'
+  ]
+};
