b699e1e0971155a2ca78afc082bce2e69635d931
Grant Fuellenbach <grant.fuellenbach@gmail.com>
2024-06-26 10:19:11 -0600

    Update ChecklistItem.js

--- src/components/ChecklistItem.js
+++ src/components/ChecklistItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Checkbox } from '@/components/ui/checkbox';
+import { Checkbox } from '@radix-ui/react-checkbox';
 
 const ChecklistItem = ({ item, onToggle, isChecked }) => (
   <div className="flex items-center space-x-2 mb-2">
